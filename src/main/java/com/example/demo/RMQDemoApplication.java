bc00d3a1780d6107d70058524d2a7398bc2e8f7d
liaohexiang <liaohexiang@gmail.com>
2023-04-04 10:14:41 +0800

    update

---
+++ src/main/java/com/example/demo/RMQDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+@SpringBootApplication
+public class RMQDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
