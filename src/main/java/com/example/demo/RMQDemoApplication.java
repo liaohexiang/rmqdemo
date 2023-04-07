c90b7500d32ba4c69b7505c974fe04b37d3c3094
liaohexiang <liaohexiang@gmail.com>
2023-04-07 15:59:32 +0800

    update

--- src/main/java/com/example/demo/RMQDemoApplication.java
+++ src/main/java/com/example/demo/RMQDemoApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableMBeanExport;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class RMQDemoApplication {
 
 	public static void main(String[] args) {
